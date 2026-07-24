e5a8d10b4a6e8254bdafa27668eab5208d87f11c
Artem Jaku <artemkudliuk@gmail.com>
2026-07-24 19:52:24 +0300

    Add contact badge button 'Зв'язатися з нами' above hero main title

--- public/wp-content/themes/bamboo/assets/js/hero-nextweb.js
+++ public/wp-content/themes/bamboo/assets/js/hero-nextweb.js
@@ -53,7 +53,7 @@ window.initHeroAnimation = function() {
       }
     });
 
-    if (title)    tl.to(title,    { y: -160, opacity: 0, duration: 2.0, ease: 'power1.inOut' }, 0);
+    if (titleWrap) tl.to(titleWrap, { y: -160, opacity: 0, duration: 2.0, ease: 'power1.inOut' }, 0);
     if (header)   tl.to(header,   { autoAlpha: 0, y: -24, duration: 1.5, ease: 'power1.inOut' }, 0);
     if (subtitle) tl.to(subtitle, { opacity: 1, y: 0, pointerEvents: 'auto', duration: 1.8, ease: 'power1.out' }, 1.5);
     if (cta)      tl.to(cta,      { opacity: 1, y: 0, pointerEvents: 'auto', duration: 1.8, ease: 'power1.out' }, 2.5);
